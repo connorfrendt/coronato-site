73e1a4aa38be0f24523d7e1e59ca82b6634bd74a
Connor Frendt <connorfrendt@gmail.com>
2025-12-21 13:41:08 -0800

    header opens and closes

--- src/components/InfoBar.vue
+++ src/components/InfoBar.vue
@@ -1,6 +1,38 @@
 <template>
     <div>
-        <div class="mobile-menu">MENU</div>
+        <!-- MOBILE HEADER -->
+        <div class="mobile-menu-wrapper">
+            <div class="menu-title" @click="menuOpener">MENU</div>
+            <div class="inner-menu-wrapper" v-show="isMenuOpen">
+                <div class="row">
+                    <div class="col">
+                        <RouterLink class="info-bar-button" to="/">Home</RouterLink>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <RouterLink class="info-bar-button" to="/about-me">About Us</RouterLink>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <RouterLink class="info-bar-button" to="/TBD">Book an Appointment</RouterLink>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <RouterLink class="info-bar-button" to="/contact-us">Contact Us</RouterLink>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <RouterLink class="info-bar-button" to="/reviews">Reviews</RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- DESKTOP HEADER -->
         <div class="container-fluid">
             <div class="row row-cols-2 row-cols-md-5 info-bar-wrapper">
                 <div class="col">
@@ -23,7 +55,17 @@
     </div>
 </template>
 
-<script></script>
+<script setup>
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+function menuOpener() {
+    console.log('Menu Open? ', isMenuOpen.value);
+    isMenuOpen.value = !isMenuOpen.value;
+    console.log('Menu Open? ', isMenuOpen.value);
+}
+</script>
 
 <style>
     @import '../scss/info-bar.scss';
